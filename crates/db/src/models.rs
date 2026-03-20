6c2572595924bea7397c4359e76eec1eca9f4926
Lucas Rimfrost <lucas.rimfrost@gmail.com>
2026-03-20 22:52:02 +0100

    Password hash will now be omitted from the output when serializing

--- crates/db/src/models.rs
+++ crates/db/src/models.rs
@@ -11,6 +11,7 @@ pub struct User {
     pub id: Uuid,
     pub username: String,
     pub email: String,
+    #[serde(skip_serializing)]
     pub password_hash: String,
     pub created_at: DateTime<Utc>,
 }
